6149e26a1565f03cdf4e38df4e77e4b11195d0ae
MarcosAlanya19 <103856433+MarcosAlanya19@users.noreply.github.com>
2023-05-07 23:15:18 -0500

    UPDATE: ConctacHomePage - Se finalizo la coneccion con un correo de llegada

--- components/organisms/ContactHomePage.tsx
+++ components/organisms/ContactHomePage.tsx
@@ -1,14 +1,47 @@
-import { FC, useState } from 'react';
+import axios from 'axios';
+import { ErrorMessage, Field, Form, Formik, FormikHelpers } from 'formik';
+import { FC } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import * as Yup from 'yup';
 import { Button, Section } from '../atom';
 
 interface Props {
   id: string;
 }
 
+type FormValues = {
+  name: string;
+  email: string;
+  message: string;
+};
+
+const validationSchema = Yup.object({
+  name: Yup.string().required('Campo obligatorio'),
+  email: Yup.string().email('Email inválido').required('Campo obligatorio'),
+  message: Yup.string().required('Campo obligatorio'),
+});
+
+const initialValues = {
+  name: '',
+  email: '',
+  message: '',
+};
+
 export const ContactHomePage: FC<Props> = ({ ...props }) => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const handleSubmit = async (
+    values: typeof initialValues,
+    { resetForm }: FormikHelpers<FormValues>
+  ) => {
+    try {
+      console.log('Email enviado');
+      await axios.post('/api/sendEmail', values);
+      resetForm();
+      toast.success('¡Email enviado con éxito!');
+    } catch (error) {
+      console.error('Error al enviar el email:', error);
+    }
+  };
 
   return (
     <Section id={props.id} title='Contacto'>
@@ -22,49 +55,58 @@ export const ContactHomePage: FC<Props> = ({ ...props }) => {
             muted
           ></video>
         </div>
-        <form>
-          <div className='mb-4'>
-            <label htmlFor='name' className='block font-medium mb-1'>
-              Nombre
-            </label>
-            <input
-              type='text'
-              id='name'
-              name='name'
-              className='w-full px-3 py-2 border border-gray-300 rounded'
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div className='mb-4'>
-            <label htmlFor='email' className='block font-medium mb-1'>
-              Email
-            </label>
-            <input
-              type='email'
-              id='email'
-              name='email'
-              className='w-full px-3 py-2 border border-gray-300 rounded'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className='mb-4'>
-            <label htmlFor='message' className='block font-medium mb-1'>
-              Mensaje
-            </label>
-            <textarea
-              id='message'
-              name='message'
-              rows={4}
-              className='w-full px-3 py-2 border border-gray-300 rounded'
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            ></textarea>
-          </div>
-          <Button text='Enviar' type='submit' />
-        </form>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <div className='mb-4'>
+              <label htmlFor='name' className='block font-medium mb-1'>
+                Nombre
+              </label>
+              <Field
+                type='text'
+                id='name'
+                name='name'
+                className='w-full px-3 py-2 border border-gray-300 rounded'
+              />
+              <ErrorMessage name='name' component='div' className='text-red-500' />
+            </div>
+            <div className='mb-4'>
+              <label htmlFor='email' className='block font-medium mb-1'>
+                Email
+              </label>
+              <Field
+                type='email'
+                id='email'
+                name='email'
+                className='w-full px-3 py-2 border border-gray-300 rounded'
+              />
+              <ErrorMessage name='email' component='div' className='text-red-500' />
+            </div>
+            <div className='mb-4'>
+              <label htmlFor='message' className='block font-medium mb-1'>
+                Mensaje
+              </label>
+              <Field
+                as='textarea'
+                id='message'
+                name='message'
+                rows={4}
+                className='w-full px-3 py-2 border border-gray-300 rounded resize-none'
+              />
+              <ErrorMessage
+                name='message'
+                component='div'
+                className='text-red-500'
+              />
+            </div>
+            <Button text='Enviar' type='submit' />
+          </Form>
+        </Formik>
       </div>
+      <ToastContainer />
     </Section>
   );
 };
---
+++ pages/api/sendEmail.ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'asesoralanya19@gmail.com',
+    pass: 'gwmbyihnqgsjfgba',
+  },
+});
+
+transporter.set('sendmail', true);
+transporter.set('newline', 'unix');
+transporter.set('path', '/usr/sbin/sendmail');
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { name, email, message } = req.body;
+
+  try {
+    await transporter.sendMail({
+      from: email,
+      to: 'asesoralanya19@gmail.com',
+      subject: 'CONTACTO PORTAFOLIO',
+      text: `Nombre: ${name}\nEmail: ${email}\nMensaje: ${message}`,
+    });
+
+    res.status(200).json({ message: 'Correo electrónico enviado exitosamente' });
+  } catch (error) {
+    console.error('Error al enviar el correo electrónico:', error);
+    res.status(500).json({ message: 'Error al enviar el correo electrónico' });
+  }
+}
