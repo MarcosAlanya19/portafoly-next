e562a1993b37ee0b69734fa57b7df800ddb2e583
MarcosAlanya19 <103856433+MarcosAlanya19@users.noreply.github.com>
2023-05-08 00:44:57 -0500

    UPDATE - Button - Se agrego el onClick

--- components/atom/Button.tsx
+++ components/atom/Button.tsx
@@ -10,6 +10,7 @@ export const Button: FC<IButton> = ({ ...props }) => {
     <button
       className={`${props.className} dark:bg-sky-500 bg-amber-500 transition-colors shadow-md dark:shadow-sky-500/10 shadow-amber-500/10 dark:hover:bg-sky-400 hover:bg-amber-400 text-white font-medium tracking-wide py-2 px-4 rounded-lg`}
       id='btn'
+      onClick={props.onClick}
     >
       {props.text}
     </button>
