281c4bb4335aa16088dffea8f4827c1cdea8d281
MarcosAlanya19 <103856433+MarcosAlanya19@users.noreply.github.com>
2023-05-11 00:00:49 -0500

    ADD:se agregaron los metadatas

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -2,6 +2,7 @@ import { VerticalText } from '@/components/atom';
 import { Footer, Navbar } from '@/components/organisms';
 import { JSXElement } from '@/interface/all.interface';
 import { useThemeStore } from '@/store/useStore';
+import { NextSeo } from 'next-seo';
 import { FC } from 'react';
 
 interface Props {
@@ -11,9 +12,71 @@ interface Props {
 
 export const Layout: FC<Props> = ({ ...props }) => {
   const { isDarkMode } = useThemeStore();
+  const pageKeywords = [
+    'Marcos Alanya',
+    'Marcos Alanya Pacheco',
+    'Desarrollador Fullstack',
+    'Portafolio',
+    'Desarrollo web',
+    'Frontend',
+    'Backend',
+    'Aplicaciones web',
+    'Programador',
+    'Fullstack',
+    'Frontend',
+    'Estudiante',
+    'Ingeniero de sistemas',
+    'Proyectos',
+    'Experiencia',
+    'Habilidades',
+    'Tecnologías',
+    'Diseño web',
+    'Desarrollo web',
+    'Aplicaciones',
+    'Sitios web',
+  ];
 
   return (
     <>
+      <NextSeo
+        title='Marcos Alanya - Portafolio'
+        description='Soy Marcos, un desarrollador fullstack con experiencia en la creación de aplicaciones web. Explora mi portafolio para ver mis proyectos, habilidades en desarrollo frontend y backend, y mi pasión por la tecnología.'
+        canonical='https://marcos-alanya-portafolio.vercel.app/'
+        additionalMetaTags={[
+          {
+            property: 'keywords',
+            content: pageKeywords.join(', '),
+          },
+          {
+            property: 'robots',
+            content: 'index,follow',
+          },
+        ]}
+        openGraph={{
+          url: 'https://marcos-alanya-portafolio.vercel.app/',
+          title: 'Portafolio Marcos Alanya Pacheco',
+          description:
+            'Soy Marcos, un desarrollador fullstack con experiencia en la creación de aplicaciones web. Explora mi portafolio para ver mis proyectos, habilidades en desarrollo frontend y backend, y mi pasión por la tecnología.',
+          images: [
+            {
+              url: 'https://lh3.googleusercontent.com/pw/AJFCJaXnWYKCg3tI1K8WbxFuVAsymSILKcXaKyYO4PU3_zMC0GKDjZugpg9Fr86_wSkw7gSW0ZQngIp9ySnSZztEUxww3iV8QLj88-tSdustGEXreYUxYMk9_lJKHe0M52T706d6wPPPs6zeLiTmBHbHZrwozw=w778-h973-s-no?authuser=0',
+              width: 900,
+              height: 800,
+              alt: 'first-image',
+              type: 'portafolio',
+            },
+            {
+              url: 'https://lh3.googleusercontent.com/pw/AJFCJaVjq0N3ygQx9Bo5T8LBGU2tOdbE6FTAmZ5epTmsatYOlUzBfdl2afb86tZ0eh1kSSiK_vgV9lG2IUxxDrjybBHE4mhVAoGXJSFrTBbYnlEE0lzq426BSJQ5nFw3--O60VTRvTun2Bzd4l8aQVV6Xv2CGw=w778-h973-s-no?authuser=0',
+              width: 900,
+              height: 800,
+              alt: 'second-image',
+              type: 'portafolio',
+            },
+          ],
+          siteName: 'SiteName',
+        }}
+      />
+
       <main className={`${isDarkMode ? 'dark' : ''}`}>
         <VerticalText
           text='marcosalanya19@gmail.com'
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
+import { Navbar } from '@/components/organisms'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
