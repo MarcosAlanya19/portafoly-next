3c22f9f17d3e2d900dcedd81a52d080d28c47009
MarcosAlanya19 <103856433+MarcosAlanya19@users.noreply.github.com>
2023-05-10 23:00:49 -0500

    ADD: title de la page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,13 +5,12 @@ export default function Document() {
   return (
     <Html lang="es">
       <Head>
-        <title>Marcos - Portafolio</title>
         <meta property='og:title' content='Marcos - Portafolio' />
         <meta property='og:type' content='portafolio' />
         <meta
           property='og:url'
           content='https://marcos-alanya-portafolio.vercel.app/'
-        />
+          />
         <meta
           property='og:image'
           content='https://lh3.googleusercontent.com/pw/AJFCJaXnWYKCg3tI1K8WbxFuVAsymSILKcXaKyYO4PU3_zMC0GKDjZugpg9Fr86_wSkw7gSW0ZQngIp9ySnSZztEUxww3iV8QLj88-tSdustGEXreYUxYMk9_lJKHe0M52T706d6wPPPs6zeLiTmBHbHZrwozw=w778-h973-s-no?authuser=0'
@@ -19,12 +18,13 @@ export default function Document() {
         <meta
           property='og:description'
           content='Soy Marcos, un desarrollador fullstack con experiencia en la creación de aplicaciones web. Explora mi portafolio para ver mis proyectos, habilidades en desarrollo frontend y backend, y mi pasión por la tecnología.'
-        />
+          />
         <meta
           property='og:keywords'
           content='Marcos Alanya,Marcos Alanya Pacheco,Desarrollador Fullstack, Portafolio, Desarrollo web, Frontend, Backend, Aplicaciones web, Programador, Fullstack, Frontend, Estudiante, Ingeniero de sistemas, Proyectos, Experiencia, Habilidades, Tecnologías, Diseño web, Desarrollo web, Aplicaciones, Sitios web'
-        />
+          />
         <meta name='robots' content='index,follow' />
+        <title>MarcosAlanya-Portafolio</title>
       </Head>
       <body>
         <Main />
